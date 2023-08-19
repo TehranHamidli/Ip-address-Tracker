f0bdc3be505a64ed3897184f4ebbf015e9850a5e
Your Name <you@example.com>
2023-08-19 05:48:10 +0400

    ip-address

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const isp = document.querySelector(".country_calling_code");
 const ipAddressInfromation = document.querySelector(".ip-address-information");
 const blank = document.querySelector(".blank");
 
- function getLocation() {
+function getLocation() {
     ipAdress = inputSearch.value;
 
- fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_un4vr6AHUaq94EzSdx6SQ6HysUWHs&ipAddress=${ipAdress}&domain=${ipAdress}`)
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_un4vr6AHUaq94EzSdx6SQ6HysUWHs&ipAddress=${ipAdress}&domain=${ipAdress}`)
         .then(response => response.json())
         .then(data => {
             ip.innerHTML = data.ip
@@ -20,9 +20,7 @@ const blank = document.querySelector(".blank");
             isp.innerHTML = data.isp;
             initMap(data.location.lat, data.location.lng)
         })
-        .catch(error => {
-            console.error("An error occurred:", error);
-        });
+
 }
 getLocation()
 
@@ -31,8 +29,12 @@ btnSearch.addEventListener("click", getLocation)
 function initMap(lat, lng) {
     map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
-        center: { lat, lng },
+        center: {
+            lat,
+            lng
+        },
         mapTypeControl: false,
     });
     geocoder = new google.maps.Geocoder();
 }
+
